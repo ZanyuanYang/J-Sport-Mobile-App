e8b7c52f562a559f28de04688c517f8837b833f6
wangyan66 <819156618@qq.com>
2021-11-06 18:17:28 -0700

    Add: now send sticker page will load all stickers in Firebase

--- app/src/main/java/edu/neu/madcourse/team_j_sport/UserSendStickerActivity.java
+++ app/src/main/java/edu/neu/madcourse/team_j_sport/UserSendStickerActivity.java
@@ -1,56 +1,77 @@
 package edu.neu.madcourse.team_j_sport;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.GridView;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
 import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserSendStickerActivity extends AppCompatActivity {
     private static final String TAG = "UserSendStickerActivity";
-    private static final int STICKER_NUMBER = 6;
+    private GridView stickerGridView;
+    private List<StickerGridViewCell> cellList;
+    private List<StorageReference> refList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_send_sticker);
+        initView();
+    }
+    public void initView(){
+        refList = new ArrayList<>();
         FirebaseStorage storage = FirebaseStorage.getInstance();
         // Create a storage reference from our app
         StorageReference storageRef = storage.getReference();
-        StorageReference[] refArray = new StorageReference[STICKER_NUMBER + 1];
-        for (int i = 1; i <= STICKER_NUMBER; i++) {
-            refArray[i] = storageRef.child("sticker_" + String.valueOf(i) + ".png");
-        }
+        storageRef.listAll()
+                .addOnSuccessListener(new OnSuccessListener<ListResult>() {
+                    @Override
+                    public void onSuccess(ListResult listResult) {
+                        for (StorageReference prefix : listResult.getPrefixes()) {
+                            // All the prefixes under listRef.
+                            // You may call listAll() recursively on them.
+                            Log.w(TAG, "!!!"+ prefix.toString());
+                        }
 
-        ImageView imageView1 = findViewById(R.id.sticker1);
-        ImageView imageView2 = findViewById(R.id.sticker2);
-        ImageView imageView3 = findViewById(R.id.sticker3);
-        ImageView imageView4 = findViewById(R.id.sticker4);
-        ImageView imageView5 = findViewById(R.id.sticker5);
-        ImageView imageView6 = findViewById(R.id.sticker6);
-        // Download directly from StorageReference using Glide
-        // (See MyAppGlideModule for Loader registration)
-        Glide.with(this /* context */)
-                        .load(refArray[1])
-                        .into(imageView1);
-        Glide.with(this /* context */)
-                .load(refArray[2])
-                .into(imageView2);
-        Glide.with(this /* context */)
-                .load(refArray[3])
-                .into(imageView3);
-        Glide.with(this /* context */)
-                .load(refArray[4])
-                .into(imageView4);
-        Glide.with(this /* context */)
-                .load(refArray[5])
-                .into(imageView5);
-        Glide.with(this /* context */)
-                .load(refArray[6])
-                .into(imageView6);
+                        for (StorageReference item : listResult.getItems()) {
+                            // All the items under listRef.
+                            refList.add(item);
+                            Log.w(TAG, " "+ item.toString());
 
+                        }
+                        loadStickers();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Uh-oh, an error occurred!
+                        Log.w(TAG, "Failed.");
+                    }
+                });
     }
+    public void loadStickers(){
+        stickerGridView = (GridView) findViewById(R.id.SendSticker_GridView);
+        cellList = new ArrayList<>();
+
+        for (int i = 0; i < refList.size(); i++) {
+            StickerGridViewCell cell = new StickerGridViewCell();
+            cell.imageReference = refList.get(i);
+            cellList.add(cell);
+        }
+        //        .notifyItemInserted(0)
+        stickerGridView.setAdapter(new StickerGridViewAdapter(this, cellList));
+    }
+
 }
