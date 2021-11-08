15ab308714dd17ac46ec6b6ec442164f1819293d
Shane Z <22213632+ShaneZTH@users.noreply.github.com>
2021-11-08 15:30:20 -0800

    feat: FCM subscribe and unsubscribe

--- app/src/main/java/edu/neu/madcourse/team_j_sport/UserPageActivity.java
+++ app/src/main/java/edu/neu/madcourse/team_j_sport/UserPageActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
