398a4d08d566058924c06ab0c32db26a6a081a28
ZanyuanYang <59616360+ZanyuanYang@users.noreply.github.com>
2021-11-08 15:26:52 -0800

    Add: filter username in login page

--- app/src/main/java/edu/neu/madcourse/team_j_sport/UtilsFunction.java
+++ app/src/main/java/edu/neu/madcourse/team_j_sport/UtilsFunction.java
@@ -35,4 +35,28 @@ public class UtilsFunction {
         });
         return id[0];
     }
+
+    // check Username Exists Or Not
+    public static boolean checkUserExistsOrNot(String username){
+        boolean[] checked = {true};
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = database.getReference().child("Users");
+        myRef.addValueEventListener(new ValueEventListener(){
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot){
+
+                for (DataSnapshot userSnapshot: dataSnapshot.getChildren()) {
+                    if(userSnapshot.child("username").getValue().equals(username)){
+                        checked[0] = false;
+                    }
+                }
+
+            }
+            @Override
+            public void onCancelled(@NotNull DatabaseError databaseError){
+
+            }
+        });
+        return checked[0];
+    }
 }
