5b0ece430876bb8d14474f654d48d594dc99095a
Shane Z <22213632+ShaneZTH@users.noreply.github.com>
2021-12-04 22:28:48 -0800

    Add: onClickListners for middle section's textView and arrows

--- app/src/main/java/edu/neu/madcourse/team_j_sport/navi_bar/MeFragment.java
+++ app/src/main/java/edu/neu/madcourse/team_j_sport/navi_bar/MeFragment.java
@@ -1,5 +1,6 @@
 package edu.neu.madcourse.team_j_sport.navi_bar;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
@@ -9,10 +10,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import edu.neu.madcourse.team_j_sport.EventList.AddEvent;
 import edu.neu.madcourse.team_j_sport.R;
+import edu.neu.madcourse.team_j_sport.about_me.ChangePasswordActivity;
+import edu.neu.madcourse.team_j_sport.about_me.MyEventsActivity;
+import edu.neu.madcourse.team_j_sport.about_me.MyPostsActivity;
 
 
-public class MeFragment extends Fragment {
+public class MeFragment extends Fragment implements View.OnClickListener {
 
     View view;
     SharedPreferences sharedPreferences;
@@ -25,6 +30,37 @@ public class MeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_me, container, false);
+        view =  inflater.inflate(R.layout.fragment_me, container, false);
+        view.findViewById(R.id.tv_my_posts).setOnClickListener(this::onClick);
+        view.findViewById(R.id.tv_my_events).setOnClickListener(this::onClick);
+        view.findViewById(R.id.tv_change_password).setOnClickListener(this::onClick);
+        view.findViewById(R.id.iv_arrow_1).setOnClickListener(this::onClick);
+        view.findViewById(R.id.iv_arrow_2).setOnClickListener(this::onClick);
+        view.findViewById(R.id.iv_arrow_3).setOnClickListener(this::onClick);
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        switch (view.getId()){
+            case R.id.tv_my_posts:
+            case R.id.iv_arrow_1:
+                intent = new Intent(getActivity(), MyPostsActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.tv_my_events:
+            case R.id.iv_arrow_2:
+                intent = new Intent(getActivity(), MyEventsActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.tv_change_password:
+            case R.id.iv_arrow_3:
+                intent = new Intent(getActivity(), ChangePasswordActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
