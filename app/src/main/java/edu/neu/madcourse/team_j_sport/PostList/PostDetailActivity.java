d84981bc674f19204b764e60750601353119316c
Waysoning <wayson1225@outlook.com>
2021-12-09 15:33:42 -0800

    ADD: add the delete post feature

--- app/src/main/java/edu/neu/madcourse/team_j_sport/PostList/PostDetailActivity.java
+++ app/src/main/java/edu/neu/madcourse/team_j_sport/PostList/PostDetailActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import edu.neu.madcourse.team_j_sport.R;
 
 public class PostDetailActivity extends AppCompatActivity {
+
+    private String postKey;
+
+    private final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private final DatabaseReference myRef = database.getReference();
 
     Button btnDelete;
 
@@ -17,17 +26,18 @@ public class PostDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_detail);
 
-        initView();
-
-
+        // get the postKey
         Intent intent = getIntent();
-        String postKey = intent.getStringExtra(PostHolder.POST_KEY);
-        btnDelete.setText(postKey);
+        postKey = intent.getStringExtra(PostHolder.POST_KEY);
 
-
+        initView();
     }
 
     private void initView() {
         btnDelete = findViewById(R.id.btn_post_delete);
+        btnDelete.setOnClickListener(view -> {
+            myRef.child("Posts").child(postKey).removeValue();
+            finish();
+        });
     }
 }
