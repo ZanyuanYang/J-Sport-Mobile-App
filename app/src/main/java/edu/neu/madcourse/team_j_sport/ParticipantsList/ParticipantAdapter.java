05cc11ecf684a1e28b27da4f627cb015c33b7e7a
Waysoning <wayson1225@outlook.com>
2021-12-10 02:01:36 -0800

    UI: UI the participant list

--- app/src/main/java/edu/neu/madcourse/team_j_sport/ParticipantsList/ParticipantAdapter.java
+++ app/src/main/java/edu/neu/madcourse/team_j_sport/ParticipantsList/ParticipantAdapter.java
@@ -18,7 +18,7 @@ import edu.neu.madcourse.team_j_sport.PostList.ItemPost;
 import edu.neu.madcourse.team_j_sport.PostList.PostHolder;
 import edu.neu.madcourse.team_j_sport.R;
 
-public class ParticipantAdapter extends RecyclerView.Adapter<PostHolder> {
+public class ParticipantAdapter extends RecyclerView.Adapter<ParticipantHolder> {
 
     private final ArrayList<ItemParticipant> participants;
     private final Context mContext;
@@ -30,23 +30,23 @@ public class ParticipantAdapter extends RecyclerView.Adapter<PostHolder> {
 
     @NonNull
     @Override
-    public PostHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_post, parent, false);
-        return new PostHolder(view, mContext);
+    public ParticipantHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_participant, parent, false);
+        return new ParticipantHolder(view, mContext);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull PostHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ParticipantHolder holder, int position) {
         ItemParticipant currentPost = participants.get(position);
-        
+
         holder.ivAvatar.setImageResource(R.drawable.avatar_default);
         StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("avatars/" + currentPost.getUid() +".jpg");
         Glide.with(mContext /* context */)
                 .load(storageReference)
                 .into(holder.ivAvatar);
 
-        holder.tvPostTitle.setText(currentPost.getUsername());
-        holder.tvPostContent.setText(currentPost.getEmail());
+        holder.tvUsername.setText(currentPost.getUsername());
+        holder.tvEmail.setText(currentPost.getEmail());
     }
 
     @Override
