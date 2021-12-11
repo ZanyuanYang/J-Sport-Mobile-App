da9c06df0980bbaeb21c64bc0d9e5474c7a311b8
Waysoning <wayson1225@outlook.com>
2021-12-10 22:58:20 -0800

    FIX: fix the bug of the delete the post in post list

--- app/src/main/java/edu/neu/madcourse/team_j_sport/CommentList/PostDetailActivity.java
+++ app/src/main/java/edu/neu/madcourse/team_j_sport/CommentList/PostDetailActivity.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
