74a75a5b17a3aa61c39e058fc6e6f3744f4a3d5b
Shane Z <22213632+ShaneZTH@users.noreply.github.com>
2021-12-10 23:28:27 -0800

    Fix: dup items in event list

--- app/src/main/java/edu/neu/madcourse/team_j_sport/navi_bar/EventFragment.java
+++ app/src/main/java/edu/neu/madcourse/team_j_sport/navi_bar/EventFragment.java
@@ -51,10 +51,11 @@ public class EventFragment extends Fragment {
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate");
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_event, container, false);
         sharedPreferences = getActivity().getSharedPreferences("login", MODE_PRIVATE);
-        initEventList();
+//        initEventList();
         initFloatingBtn();
         return view;
     }
